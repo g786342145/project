7ee90d5b87a9dbb727c642a25f646a775f1794d8
g786342145 <786342145@qq.com>
2020-03-04 11:01:41 +0800

    新的项目

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'  //引入vue
+import VueRouter from 'vue-router'  //引入路由
+
+Vue.use(VueRouter)  //使用vue对路由进行注册
+
+const router = new VueRouter({  //创建一个路由实例
+	routes:[
+		
+	]
+})
+
+export default router  //抛出理由模块
